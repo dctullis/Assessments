4a651dc18dcd58864f6ba850ae15bfb9fac03a8b
David Tullis <45954871+dctullis@users.noreply.github.com>
2021-04-09 14:47:09 -0500

    Update tuple-challenge.cpp

--- tuple-challenge.cpp
+++ tuple-challenge.cpp
@@ -5,7 +5,7 @@
 // Notes: This program will create a Tuple, no matter what's inside it,  
 //        and attempt to add all that is numeric to a global variable, 
 //        and cast all that is string-castable to a global variable.
-//        Whatever is not accepted will be counted. The end result will
+//        Whatever is not accepted will be counted. The end result 
 //        will be printed out by main().
 // 
 //        This program was built and tested with C++17.      
